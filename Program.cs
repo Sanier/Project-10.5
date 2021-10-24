e41d882a51dd3c31168848e9e78779caf961b252
Sanier <Sanier20@mail.ru>
2021-10-24 12:24:50 +0300

    Мини-калькулятор с исключениями

--- Program.cs
+++ Program.cs
@@ -4,18 +4,80 @@ namespace Project_10._5
 {
     class Program
     {
+
+        static ICalculate Sum { get; set; }
         static void Main(string[] args)
         {
 
-            try
-            {
+            Sum = new Sum();
 
+            while (true)
+            {
+                try
+                {
+                    int a = int.Parse(Console.ReadLine());
+                    string sign = Console.ReadLine();
+                    int b = int.Parse(Console.ReadLine());
+                    
+
+                    switch (sign)
+                    {
+                        case "/":
+                            {
+                                Console.WriteLine(Sum.Div(a, b));
+
+                                break;
+                            }
+
+                        case "*":
+                            {
+                                Console.WriteLine(Sum.Mult(a, b));
+
+                                break;
+                            }
+
+                        case "+":
+                            {
+                                Console.WriteLine(Sum.Sum(a, b));
+
+                                break;
+                            }
+
+                        case "-":
+                            {
+                                Console.WriteLine(Sum.Sub(a, b));
+
+                                break;
+                            }
+
+                        default:
+                            {
+                                throw new ArgumentException();
+                            }
+                    }
+                }
+
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Деление на ноль!");
+                }
+
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Сорян, слишком большоооооооое число. Не могу показать");
+                }
+
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Так делать низя");
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка {0}", ex);
+                }
             }
 
-            catch()
-            {
-
-            }
         }
 
         public interface ICalculate
--- Sum.cs
+++ Sum.cs
@@ -7,21 +7,21 @@ using static Project_10._5.Program;
 
 namespace Project_10._5
 {
-    class Sum : ICalculate
+    public class Sum : ICalculate
     {
         ICalculate sum { get; }
 
-        public int Div(int a, int b)
+        int ICalculate.Div(int a, int b)
         {
             return a / b;
         }
 
-        public int Mult(int a, int b)
+        int ICalculate.Mult(int a, int b)
         {
             return a * b;
         }
 
-        public int Sub(int a, int b)
+        int ICalculate.Sub(int a, int b)
         {
             return a - b;
         }
