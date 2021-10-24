bbae86a21e55baff5f8ca83d27b1df30e5c1714e
Sanier <Sanier20@mail.ru>
2021-10-24 11:19:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project_10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+            }
+
+            catch()
+            {
+
+            }
+        }
+
+        public interface ICalculate
+        {
+            int Sum(int a, int b);
+
+            int Div(int a, int b);
+
+            int Mult(int a, int b);
+
+            int Sub(int a, int b);
+        }
+    }
+}
---
+++ Sum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Project_10._5.Program;
+
+namespace Project_10._5
+{
+    class Sum : ICalculate
+    {
+        ICalculate sum { get; }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        int ICalculate.Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
